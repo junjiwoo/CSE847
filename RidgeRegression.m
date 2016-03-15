ccf9a5bd1fde9452988a5b14f520163f9fcce7e5
junjiwoo <zhiyuquanfrank@gmail.com>
2016-03-14 22:17:38 -0400

    Update RidgeRegression.m

--- RidgeRegression.m
+++ RidgeRegression.m
@@ -7,14 +7,15 @@ function pred = RidgeRegression(train_x, train_y, test_x, lambda)
 %% test dimensions
 if size(train_x, 2) ~= size(test_x, 2)
     fprintf('\nColumns of train data and test data are unequal\n');
-    return
+    return;
+end
 if size(train_x, 1) ~= size(train_y, 1)
     fprintf('\nRows of train data and target value are unequal\n');
-    return
+    return;
+end
 
 %% training
 M = size(train_x, 1);
-N = size(train_x, 2);
 [U, D, V] = svd(train_x);
 w = zeros(1, M);
 
@@ -22,10 +23,11 @@ for i = 1: M
     sigma_i = D(i, i);
     u_i = U(:, i);
     v_i = V(:, i);
-    w = w + ((sigma_i * u_i * train_y)/(sigma_i^2 + lambda)) * v_i
+    w = w + ((sigma_i * u_i * train_y)/(sigma_i^2 + lambda)) * v_i;
 end
 
 %% predicting
-pred = test_x * w
+pred = test_x * w;
 
 end
+
